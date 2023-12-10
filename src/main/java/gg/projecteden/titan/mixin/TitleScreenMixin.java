2491ce4b1460244177a21403bac0b47011da1c10
Blast-MC <cjblanton2@gmail.com>
2023-12-10 12:36:40 -0500

    1.20.4

--- src/main/java/gg/projecteden/titan/mixin/TitleScreenMixin.java
+++ src/main/java/gg/projecteden/titan/mixin/TitleScreenMixin.java
@@ -7,7 +7,7 @@ import gg.projecteden.titan.update.UpdateStatus;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.multiplayer.ConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.tooltip.Tooltip;
